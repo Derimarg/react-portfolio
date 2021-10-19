31b452886cda6742918eeca94f0d1e38ab871d42
Derimar Gray <derimargray@gmail.com>
2021-10-19 02:49:33 -0500

    add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,40 @@
 import React from "react";
 
 function Footer() {
+  const icons = [
+    {
+      name: "fab fa-github-alt",
+      url: "https://github.com/Derimarg",
+    },
+    {
+      name: "fab fa-linkedin",
+      url: "https://www.linkedin.com/in/derimar-gray-676275132/",
+    },
+    {
+      name: "fab fa-stack-overflow",
+      url: "https://stackoverflow.com/users/15126049/derimar-gray",
+    },
+  ];
+
+
   return (
     // Footer
-    <footer class="bg-light py-5">
-      <div class="container px-4 px-lg-5">
-        <div class="small text-center text-muted">
-          Copyright &copy; 2021 - Derimar Gray
+    <footer className="footer">
+      <div className="container px-4 px-lg-5">
+        <div className=" text-center text-muted">
+          {icons.map((icon) => (
+            <a
+              href={icon.url}
+              key={icon.name}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className={`icons ${icon.name}`}></i>
+            </a>
+          ))}
+        </div>
+        <div className="small text-center text-muted">
+          &copy; 2021 - Derimar Gray
         </div>
       </div>
     </footer>
