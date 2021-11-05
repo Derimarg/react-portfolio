c38fa9dd0cc3f82bd38c75e4c6eb13d0dad8738a
Derimar Gray <derimargray@gmail.com>
2021-11-05 15:56:30 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import { themes } from "./theme";
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
-
   return (
-    <ThemeProvider theme={themes[theme]} >
-      <>
-        <div theme={themes[theme]} setTheme={setTheme}>
-          <Header />
-          <Footer />
-        </div>
-      </>
-    </ThemeProvider>
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BaseProvider, LightTheme } from "baseui";
-import { Provider as StyletronProvider } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
 import "./index.css";
 import "../src/scripts.js";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
-const engine = new Styletron();
 ReactDOM.render(
-  <StyletronProvider value={engine}>
-    <BaseProvider theme={LightTheme}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BaseProvider>
-  </StyletronProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
